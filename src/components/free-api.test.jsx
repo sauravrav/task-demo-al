3da77f02b9668d4ad8ab6607aa7944e96f6d700e
sauravb <s.bhatta2383@gmail.com>
2023-04-27 01:18:47 +0545

    Fixed vercel deployment error

--- src/components/free-api.test.jsx
+++ src/components/free-api.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import Joke from './free-api';
+import Joke from '../pages/free-api';
 
 jest.mock('axios', () => ({
   get: jest.fn(() =>
